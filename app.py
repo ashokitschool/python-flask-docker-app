1f4b5738f39a6eb5d0d6244e6480e74f8d95e0a8
Ashok IT <30829130+ashokitschool@users.noreply.github.com>
2022-11-30 07:23:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask inside Docker!!"
+    return "Welcome to Python Flask Application - Ashok IT"
 
 
 if __name__ == "__main__":
